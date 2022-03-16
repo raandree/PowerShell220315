5d17c4e5f7a41d2460b99d1c8743ddc1eda579d0
Raimund Andree <r.andree@live.com>
2022-03-16 17:45:30 +0100

    Added C# 'Car' project

---
+++ Car/Car/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My
+{
+    public class Car
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public bool IsConvertible { get; set; }
+
+        public double SpeedMph {  get { return Speed / 1.6; } }        
+
+        public void Accelerate(int km)
+        {
+            Speed += km;
+        }
+        public void Brake(int km)
+        {
+            Speed -= km;
+        }
+    }
+}
